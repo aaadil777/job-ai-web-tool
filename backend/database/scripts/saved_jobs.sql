a655c06873bd870ef201e830ee838cb828aa2000
Napoleon Awunglefac <122139479+Nawunglefac@users.noreply.github.com>
2025-10-26 20:24:37 -0500

    Add SQL script for Saved Jobs table

---
+++ backend/database/scripts/saved_jobs.sql
@@ -0,0 +1,19 @@
+-- Table: SavedJobs
+
+CREATE TABLE IF NOT EXISTS saved_jobs (
+    saved_job_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    job_id INT NOT NULL,
+    date_saved DATETIME DEFAULT CURRENT_TIMESTAMP,
+    notes VARCHAR(255),
+
+    CONSTRAINT fk_savedjobs_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_savedjobs_job
+        FOREIGN KEY (job_id)
+        REFERENCES jobs(job_id)
+        ON DELETE CASCADE
+);
